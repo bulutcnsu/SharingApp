b151cd7aeb2ef3af55f7cab5303304e433ccab40
bulutcnsu <60491413+bulutcnsu@users.noreply.github.com>
2021-05-31 23:52:45 +0300

    Add files via upload

---
+++ Service-Oriented-Archiecture/Week2/week4/SharingAppC4/src/AddBidCommand.java
@@ -0,0 +1,32 @@
+import android.content.Context;
+import java.util.concurrent.ExecutionException;
+
+public class AddBidCommand extends Command {
+
+    private BidList bid_list;
+    private Bid bid;
+    private Context context;
+
+    public AddBidCommand(Bid bid) { this.bid = bid; }
+
+    // Save bid locally
+    /*public void execute(){
+        bid_list.addBid(bid);
+        super.setIsExecuted(bid_list.saveBids(context));
+    }*/
+
+    // Save the item remotely to server
+    public void execute(){
+        ElasticSearchManager.AddBidTask add_bid_task = new ElasticSearchManager.AddBidTask();
+        add_bid_task.execute(bid);
+
+
+        try {
+            if(add_bid_task.get()) {
+                bid_list.addBid(bid);
+                super.setIsExecuted(true);
+            }
+        } catch (InterruptedException | ExecutionException e) {
+            e.printStackTrace();
+            super.setIsExecuted(false);
+        }
